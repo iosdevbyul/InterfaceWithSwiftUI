d759764c91d15e0ea206d70f45bdfb1091a0a112
Jessi <sbjoo@pntbiz.com>
2022-10-19 19:35:35 +0900

    Initial Commit

---
+++ InterfaceWithSwiftUI/InterfaceWithSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  InterfaceWithSwiftUIApp.swift
+//  InterfaceWithSwiftUI
+//
+//  Created by PNT001 on 2022/10/19.
+//
+
+import SwiftUI
+
+@main
+struct InterfaceWithSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
